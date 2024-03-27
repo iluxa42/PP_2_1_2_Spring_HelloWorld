8180679d580c49ce4b963818e9480f603bd52339
iluxa42 <iluxa3650@gmail.com>
2024-03-28 01:46:11 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+
+        HelloWorld helloWorld1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorld2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(helloWorld1.getMessage());
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("helloWorld1 == helloWorld2 ? "
+                + (helloWorld1 == helloWorld2));
+        System.out.println("cat1 == cat2 ? "
+                + (cat1 == cat2));
     }
 }
